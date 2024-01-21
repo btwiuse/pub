03b98d957ab89b7fdccbc232cc86175fabecd162
navigaid <navigaid@gmail.com>
2024-01-21 18:07:40 +0000

    v0.1.2: use xz compression

--- build.rs
+++ build.rs
@@ -4,9 +4,9 @@ use std::io::copy;
 use std::io::BufReader;
 use std::path::PathBuf;
 
-use flate2::read::GzDecoder;
 use reqwest::blocking::get;
 use tar::Archive;
+use xz2::read::XzDecoder;
 
 fn main() {
     let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
@@ -16,20 +16,22 @@ fn main() {
     let lib_dir = dir.join("staticlib").join("linux").join(arch.clone());
     if lib_dir.exists() {
         println!("cargo:rustc-link-search=native={}", lib_dir.display());
-        return
+        return;
     }
-
 
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     // Download the file
-    let url = format!("https://github.com/btwiuse/pub/releases/download/v{}/staticlib.tgz", version);
+    let url = format!(
+        "https://github.com/btwiuse/pub/releases/download/v{}/staticlib.txz",
+        version
+    );
     let mut response = get(url).expect("Failed to download file");
-    let mut out = File::create(out_dir.join("staticlib.tgz")).expect("Failed to create file");
+    let mut out = File::create(out_dir.join("staticlib.txz")).expect("Failed to create file");
     copy(&mut response, &mut out).expect("Failed to copy content");
 
     // Decompress and extract the file
-    let tar_gz = File::open(out_dir.join("staticlib.tgz")).unwrap();
-    let tar = GzDecoder::new(BufReader::new(tar_gz));
+    let tar_xz = File::open(out_dir.join("staticlib.txz")).unwrap();
+    let tar = XzDecoder::new(BufReader::new(tar_xz));
     let mut archive = Archive::new(tar);
     archive.unpack(&out_dir).expect("Failed to unpack archive");
 
