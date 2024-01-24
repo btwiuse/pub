18674b3915bdb94c6dba84bbcf4fa9fa75fdcdd7
navigaid <navigaid@gmail.com>
2024-01-24 05:05:40 +0000

    verbose

--- .release.sh
+++ .release.sh
@@ -5,7 +5,7 @@ ROOT_DIR=$(dirname $(realpath -m ${0}))
 loop(){
   cd ${ROOT_DIR}
   ls -1d staticlib/*/* | tr / ' ' | while read dir os arch; do
-    bash -v "tar -cvJ $dir/$os/$arch -f staticlib-$os-$arch.txz"
+    bash -cv "tar -cvJ $dir/$os/$arch -f staticlib-$os-$arch.txz"
   done
 }
 
