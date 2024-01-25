b869f76e632a79b9f7715310f01cdc8bcde8fc77
navigaid <navigaid@gmail.com>
2024-01-25 21:22:37 +0000

    remove ./offline hack

--- offline/gen.go
+++
@@ -1,5 +0,0 @@
-package main
-
-//go:generate env CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -v -o staticlib/linux/x86_64/libpub.a -buildmode=c-archive .
-//go:generate env CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc GOOS=linux GOARCH=arm64 go build -v -o staticlib/linux/aarch64/libpub.a -buildmode=c-archive .
-//go:generate tar -cJ staticlib -f staticlib.txz
