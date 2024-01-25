cf268a5469569d3116e285f9526d671c78d99ed5
navigaid <navigaid@gmail.com>
2024-01-25 16:15:23 +0000

    remove gotip

--- pub.go
+++ pub.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/btwiuse/dl/version"
 	"github.com/btwiuse/pub/handler"
 	"github.com/webteleport/utils"
 	"github.com/webteleport/wtf"
@@ -79,10 +78,4 @@ func Run(args []string) error {
 	handler = utils.GzipMiddleware(handler)
 	handler = utils.GinLoggerMiddleware(handler)
 	return wtf.Serve("https://k0s.io", handler)
-}
-
-func RunTip(args []string) error {
-        os.Args = append([]string{"gotip"}, args...)
-        version.RunTip()
-        return nil
 }
