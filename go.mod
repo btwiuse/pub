31f21ae9734f1dd64e5d6ca625d969dbc4149ff7
navigaid <navigaid@gmail.com>
2024-01-27 13:27:52 +0000

    v0.1.14: use rclone file server

--- go.mod
+++ go.mod
@@ -3,38 +3,76 @@ module github.com/btwiuse/pub
 go 1.21.6
 
 require (
+	github.com/rclone/rclone v1.65.2
 	github.com/webteleport/utils v0.2.5
 	github.com/webteleport/wtf v0.0.8
 	k0s.io/pkg/rng v0.1.10
 )
 
 require (
+	github.com/Max-Sum/base32768 v0.0.0-20230304063302-18e6ce5945fd // indirect
+	github.com/Unknwon/goconfig v1.0.0 // indirect
+	github.com/aalpar/deheap v0.0.0-20210914013432-0cc84d79dec3 // indirect
+	github.com/abbot/go-http-auth v0.4.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/coreos/go-semver v0.3.1 // indirect
 	github.com/docker/docker v25.0.1+incompatible // indirect
 	github.com/ebi-yade/altsvc-go v0.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/go-git/go-billy/v5 v5.5.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/google/pprof v0.0.0-20230821062121-407c9e7a662f // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
 	github.com/gorilla/handlers v1.5.2 // indirect
+	github.com/iguanesolutions/go-systemd/v5 v5.1.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jzelinskie/whirlpool v0.0.0-20201016144138-0675e54bb004 // indirect
 	github.com/kataras/basicauth v0.0.3 // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20231016141302-07b5767bb0ed // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/miekg/dns v1.1.58 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/onsi/ginkgo/v2 v2.12.0 // indirect
+	github.com/pkg/xattr v0.4.9 // indirect
+	github.com/power-devops/perfstat v0.0.0-20221212215047-62379fc7944b // indirect
+	github.com/prometheus/client_golang v1.17.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/quic-go/qpack v0.4.0 // indirect
 	github.com/quic-go/quic-go v0.41.0 // indirect
 	github.com/quic-go/webtransport-go v0.6.0 // indirect
+	github.com/rfjakob/eme v1.1.2 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/rs/cors v1.10.1 // indirect
+	github.com/shirou/gopsutil/v3 v3.23.9 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966 // indirect
+	github.com/smartystreets/goconvey v1.8.1 // indirect
+	github.com/spf13/cobra v1.7.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/webteleport/auth v0.0.6 // indirect
 	github.com/webteleport/webteleport v0.3.8 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	go.uber.org/mock v0.3.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
 	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.17.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- handler/handler.go
+++ handler/handler.go
@@ -1,11 +1,23 @@
 package handler
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
+	_ "github.com/rclone/rclone/backend/local"
+	"github.com/rclone/rclone/cmd"
+	"github.com/rclone/rclone/fs"
+	libhttp "github.com/rclone/rclone/lib/http"
+	"github.com/rclone/rclone/lib/http/serve"
+	"github.com/rclone/rclone/vfs"
+	"github.com/rclone/rclone/vfs/vfsflags"
 	"github.com/webteleport/utils"
 )
 
@@ -68,12 +80,117 @@ func ResourceEmoji(s string) string {
 	}
 }
 
+func serveDir(s string) http.Handler {
+	VFS := vfs.New(cmd.NewFsSrc([]string{s}), &vfsflags.Opt)
+	t, _ := libhttp.GetTemplate("")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		remote := strings.TrimPrefix(r.URL.Path, "/")
+		// List the directory
+		node, err := VFS.Stat(r.URL.Path)
+		if err == vfs.ENOENT {
+			http.Error(w, "Directory not found", http.StatusNotFound)
+			return
+		} else if err != nil {
+			serve.Error(s, w, "Failed to list directory", err)
+			return
+		}
+		if node.IsDir() && !strings.HasSuffix(r.URL.Path, "/") {
+			http.Redirect(w, r, r.URL.Path+"/", 302)
+			return
+		}
+		if node.IsFile() {
+			entry := node.DirEntry()
+			if entry == nil {
+				http.Error(w, "Can't open file being written", http.StatusNotFound)
+				return
+			}
+			file := node.(*vfs.File)
+
+			// Set content length if we know how long the object is
+			knownSize := file.Size() >= 0
+			if knownSize {
+				w.Header().Set("Content-Length", strconv.FormatInt(node.Size(), 10))
+			}
+
+			// Set the Last-Modified header to the timestamp
+			w.Header().Set("Last-Modified", file.ModTime().UTC().Format(http.TimeFormat))
+
+			// If HEAD no need to read the object since we have set the headers
+			if r.Method == "HEAD" {
+				return
+			}
+
+			// open the object
+			in, err := file.Open(os.O_RDONLY)
+			if err != nil {
+				serve.Error(remote, w, "Failed to open file", err)
+				return
+			}
+			defer func() {
+				err := in.Close()
+				if err != nil {
+					fs.Errorf(remote, "Failed to close file: %v", err)
+				}
+			}()
+
+			// http.ServeContent can't serve unknown length files
+			if !knownSize {
+				if rangeRequest := r.Header.Get("Range"); rangeRequest != "" {
+					http.Error(w, "Can't use Range: on files of unknown length", http.StatusRequestedRangeNotSatisfiable)
+					return
+				}
+				n, err := io.Copy(w, in)
+				if err != nil {
+					fmt.Errorf("Didn't finish writing GET request (wrote %d/unknown bytes): %v", n, err)
+					return
+				}
+			}
+
+			// Serve the file
+			http.ServeContent(w, r, remote, node.ModTime(), in)
+
+			return
+		}
+
+		if !node.IsDir() {
+			http.Error(w, "Not a directory", http.StatusNotFound)
+			return
+		}
+
+		dir := node.(*vfs.Dir)
+		dirEntries, err := dir.ReadDirAll()
+		if err != nil {
+			serve.Error(s, w, "Failed to list directory", err)
+			return
+		}
+
+		// Make the entries for display
+		directory := serve.NewDirectory(remote, t)
+		for _, node := range dirEntries {
+			if vfsflags.Opt.NoModTime {
+				directory.AddHTMLEntry(node.Path(), node.IsDir(), node.Size(), time.Time{})
+			} else {
+				directory.AddHTMLEntry(node.Path(), node.IsDir(), node.Size(), node.ModTime().UTC())
+			}
+		}
+
+		sortParm := r.URL.Query().Get("sort")
+		orderParm := r.URL.Query().Get("order")
+		directory.ProcessQueryParams(sortParm, orderParm)
+
+		// Set the Last-Modified header to the timestamp
+		w.Header().Set("Last-Modified", dir.ModTime().UTC().Format(http.TimeFormat))
+
+		directory.Serve(w, r)
+	})
+}
+
 func ResourceHandler(s string) http.Handler {
 	switch {
 	case isFile(s):
 		return serveFile(s)
 	case pathExists(s):
-		return http.FileServer(http.Dir(s))
+		return serveDir(s)
 	case isPort(s):
 		return utils.ReverseProxy(s)
 	case isHostPort(s):
